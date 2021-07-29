54d41a9d6dbc993049fbc9f26a1ebb2de9dc5ecc
PavanPutha111 <83884588+PavanPutha111@users.noreply.github.com>
2021-07-30 03:22:24 +0530

    Update game.js

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ $(document).keypress(function() {
     }
 });
 
-$("input").click(function() {
+$(document).click(function() {
         $("input").css("display", "none");
         $("p").css("display","none");
 })
